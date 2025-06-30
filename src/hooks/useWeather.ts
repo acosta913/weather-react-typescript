7915749a9a069e05fac40fc081ce1f4a12bd8e1f
acosta <acostapaez913@gmail.com>
2025-06-30 00:17:42 -0500

    use type valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import type { SearchType } from "../types";
-import { z } from "zod";
+// import { z } from "zod";
+import { object, string, number, InferOutput, parse } from "valibot";
 
 // TYPE GUARD o ASSERTION
 // function isWeatherReponse(weather: unknown): weather is Weather {
@@ -15,15 +16,26 @@ import { z } from "zod";
 // }
 
 // Zod
-const Weather = z.object({
-  name: z.string(),
-  main: z.object({
-    temp: z.number(),
-    temp_max: z.number(),
-    temp_min: z.number(),
+// const Weather = z.object({
+//   name: z.string(),
+//   main: z.object({
+//     temp: z.number(),
+//     temp_max: z.number(),
+//     temp_min: z.number(),
+//   }),
+// });
+// type Weather = z.infer<typeof Weather>;
+
+// Valibot
+const WeatherSchema = object({
+  name: string(),
+  main: object({
+    temp: number(),
+    temp_max: number(),
+    temp_min: number(),
   }),
 });
-type Weather = z.infer<typeof Weather>;
+
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
@@ -48,11 +60,17 @@ export default function useWeather() {
       //   }
 
       // Zod
-      const result = Weather.safeParse(weatherResult);
-      if (result.success) {
-        console.log(data);
-      } else {
-        console.log("Respuesta mal formada");
+      //   const result = Weather.safeParse(weatherResult);
+      //   if (result.success) {
+      //     console.log(result.data);
+      //   } else {
+      //     console.log("Respuesta mal formada");
+      //   }
+
+      // Valibot
+      const result = parse(WeatherSchema, weatherResult);
+      if (result) {
+        console.log(result);
       }
     } catch (error) {
       console.log(error);
